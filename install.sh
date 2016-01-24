4e2ca967ca591ab4d7881817c57f77d244e9e74d
Micky Hulse <mickyhulse@gmail.com>
2016-01-23 22:16:37 -0800

    Install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Illustrator default template folder:
+INSTALL_DIR="$(cd /Applications/Adobe\ Illustrator*/Presets.localized/en_US/Swatches/; pwd)/@mhulse";
+
+# Remote repo zip file containing all templates:
+SOURCE_ZIP="https://github.com/mhulse/swatches/tarball/master";
+
+# Create installation folder:
+mkdir -pv "$INSTALL_DIR";
+
+# Get the zip file and extract all template files:
+curl -sS -#L "$SOURCE_ZIP" | tar -xzv --strip-components 1 -C "$INSTALL_DIR" "*.ase";
+
+# Done!
+# For more information about this script, see:
+# https://github.com/mhulse/illy-profiles
